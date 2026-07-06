df12fb708481b11ba471cdfe3bb2085f52509c18
Adam Ketchum <adam.ketchum@att.net>
2026-07-05 23:01:57 -0400

    brainfuck interpreter

---
+++ bf.cxx
@@ -0,0 +1,268 @@
+//brain fuck interpreter
+
+#include "hof.cxx"
+#include <type_traits>
+
+using ex = List<'.', List<'>', List<'<', Unit>>>;
+
+template<typename L, char Instr, typename R>
+struct InstrPtr{};
+
+template <typename L = Unit, char Cell = 0, typename R = Unit>
+struct Tape {};
+
+// Helper function for moving forward/backward through the program
+// Handles end of list
+
+template <typename InstrPtr>
+struct StepPtrRight {};
+
+template <typename IPrev, char Instr, char NextInstr, typename INext>
+struct StepPtrRight<InstrPtr<IPrev, Instr, List<NextInstr, INext>>> {
+    using type = InstrPtr<List<Instr, IPrev>, NextInstr, INext>;
+};
+
+template<typename IPrev, char Instr> 
+struct StepPtrRight<InstrPtr<IPrev, Instr, Unit>> {
+    using type = Unit;
+};
+
+template <typename InstrPtr>
+struct StepPtrLeft {};
+
+template <typename IPrev, char PrevInstr, char Instr, typename INext>
+struct StepPtrLeft<InstrPtr<List<PrevInstr, IPrev>, Instr, INext>> {
+    using type = InstrPtr<IPrev, PrevInstr, List<Instr, INext>>;
+};
+
+template <char Instr, typename INext>
+struct StepPtrLeft<InstrPtr<Unit, Instr, INext>> {
+    using type = Unit;
+};
+
+// Helper functions for moving forward/backward through the tape
+// Creates a new cell if I reached the end (Unit)
+
+template<typename Tape>
+struct StepTapeRight {};
+
+// Normal case, just move to the right 
+template <typename TPrev, char Cell, char NextCell, typename TNext>
+struct StepTapeRight<Tape<TPrev, Cell, List<NextCell, TNext>>> {
+    using type = Tape<List<Cell, TPrev>, NextCell, TNext>;
+};
+
+// End of tape, generate new cell
+template <typename TPrev, char Cell>
+struct StepTapeRight<Tape<TPrev, Cell, Unit>> {
+    using type = Tape<List<Cell, TPrev>, char{0}, Unit>;
+};
+
+template<typename Tape>
+struct StepTapeLeft{};
+
+// Normal case, just move to the right 
+template <typename TPrev, char PrevCell, char Cell, typename TNext>
+struct StepTapeLeft<Tape<List<PrevCell, TPrev>, Cell, TNext>> {
+    using type = Tape<TPrev, PrevCell, List<Cell, TNext>>;
+};
+
+// End of tape, generate new cell
+template <char Cell, typename TNext>
+struct StepTapeLeft<Tape<Unit, Cell, TNext>> {
+    using type = Tape<Unit, char{0}, List<Cell, TNext>>;
+};
+
+
+// Function to find and jump to the closing ] 
+template<typename InstrPtr, int Depth = 0>
+struct ScanRight{};
+
+template <typename IPrev, typename INext, int Depth>
+struct ScanRight<InstrPtr<IPrev, ']',  INext>, Depth> {
+    using type = std::conditional_t<Depth == 0, 
+                      typename StepPtrRight<InstrPtr<IPrev, '[', INext>>::type,
+                      typename ScanRight<typename StepPtrRight<InstrPtr<IPrev, ']', INext>>::type, Depth-1>::type
+                 >;
+};
+
+template <typename IPrev, typename INext, int Depth>
+struct ScanRight<InstrPtr<IPrev, '[', INext>, Depth> {
+    using type = typename ScanRight<typename StepPtrRight<InstrPtr<IPrev, '[', INext>>::type, Depth+1>::type;
+};
+
+template <typename IPrev, char Instr, typename INext, int Depth>
+struct ScanRight<InstrPtr<IPrev, Instr, INext>, Depth> {
+    using type = typename ScanRight<typename StepPtrRight<InstrPtr<IPrev, Instr, INext>>::type, Depth>::type;
+};
+
+template<int Depth>
+struct ScanRight<Unit, Depth> {
+    using type = Unit;
+};
+
+// Function to find and jump to the opening [
+template<typename Instrs, int Depth = 0>
+struct ScanLeft{};
+
+template <typename IPrev, typename INext, int Depth>
+struct ScanLeft<InstrPtr<IPrev, '[', INext>, Depth> {
+    using type = std::conditional_t<Depth == 0, 
+                      typename StepPtrRight<InstrPtr<IPrev, '[', INext>>::type,
+                      typename ScanLeft<typename StepPtrLeft<InstrPtr<IPrev, '[', INext>>::type, Depth-1>::type
+                 >;
+};
+
+template <typename IPrev, typename INext, int Depth>
+struct ScanLeft<InstrPtr<IPrev, ']', INext>, Depth> {
+    using type = typename ScanLeft<StepPtrLeft<InstrPtr<IPrev, ']', INext>>, Depth+1>::type;
+};
+
+template <typename IPrev, char Instr, typename INext, int Depth>
+struct ScanLeft<InstrPtr<IPrev, Instr, INext>, Depth> {
+    using type = typename ScanLeft<typename StepPtrLeft<InstrPtr<IPrev, Instr, INext>>::type, Depth>::type;
+};
+
+template<int Depth>
+struct ScanLeft<Unit, Depth> {
+    using type = Unit;
+};
+
+
+// Execute an Instruction
+template <typename Instrs, typename Tape>
+struct Step{};
+
+// PLUS
+template<typename IPrev, typename INext, typename TPrev, char Cell, typename TNext>
+struct Step<InstrPtr<IPrev, '+', INext>, Tape<TPrev, Cell, TNext>> {
+    using type = typename Step<
+        typename StepPtrRight<InstrPtr<IPrev, '+', INext>>::type,
+        Tape<TPrev, Cell+1, TNext>
+    >::type;
+};
+
+
+// MINUS
+template<typename IPrev, typename INext, typename TPrev, char Cell, typename TNext>
+struct Step<InstrPtr<IPrev, '-', INext>, Tape<TPrev, Cell, TNext>> {
+    using type = typename Step<
+        StepPtrRight<InstrPtr<IPrev, '-', INext>>, 
+        Tape<TPrev, Cell-1, TNext>
+    >::type;
+};
+
+// RIGHT
+template<typename IPrev, typename INext, typename Tape>
+struct Step<InstrPtr<IPrev, '>', INext>, Tape> {
+    using type = typename Step<
+        typename StepPtrRight<InstrPtr<IPrev, '>', INext>>::type,
+        StepTapeRight<Tape>
+    >::type;
+};
+
+// LEFT
+template<typename IPrev, char NextInstr, typename INext, typename Tape>
+struct Step<InstrPtr<IPrev, '<', List<NextInstr, INext>>, Tape> {
+    using type = typename Step<
+        typename StepPtrRight<InstrPtr<IPrev, '<', INext>>::type,
+        StepTapeLeft<Tape>
+    >::type;
+};
+
+// DOT
+template<typename IPrev, typename INext, typename TPrev, char Cell, typename TNext>
+struct Step<InstrPtr<IPrev, '.', INext>, Tape<TPrev, Cell, TNext>> {
+    using type = List<
+        Cell,
+        typename Step<
+            typename StepPtrRight<InstrPtr<IPrev, '.', INext>>::type,
+            Tape<TPrev, Cell, TNext>
+        >::type 
+    >;
+};
+
+// No , for now :(
+
+// OPEN_BRACKET 
+template<typename IPrev, typename INext, typename TPrev, char Cell, typename TNext>
+struct Step<InstrPtr<IPrev, '[', INext>, Tape<TPrev, Cell, TNext>> {
+    using type = std::conditional_t<Cell == 0,
+        typename Step<
+            typename ScanRight<
+                typename StepPtrRight<InstrPtr<TPrev, '[', TNext>>::type
+            >::type,
+            Tape<TPrev, Cell, TNext>
+        >::type,
+
+        typename Step<
+            typename StepPtrRight<InstrPtr<IPrev, '[', INext>>::type,
+            Tape<TPrev, Cell, TNext>
+        >::type
+    >;
+};
+
+// CLOSE_BRACKET
+template<typename IPrev, typename INext, typename TPrev, char Cell, typename TNext>
+struct Step<InstrPtr<IPrev, ']', INext>, Tape<TPrev, Cell, TNext>> {
+    using type = std::conditional_t<Cell != 0,
+        typename Step<
+            typename ScanLeft<
+                typename StepPtrLeft<InstrPtr<IPrev, ']', INext>>::type
+            >::type,
+            Tape<TPrev, Cell, TNext>
+        >::type,
+
+        typename Step<
+            typename StepPtrRight<InstrPtr<IPrev, ']', INext>>::type,
+            Tape<TPrev, Cell, TNext>
+        >::type 
+    >;
+};
+
+template<typename Tape>
+struct Step<Unit, Tape> {
+    using type = Unit;
+};
+
+// 33 is the first readable ascii char "!"
+using Program = InstrPtr<Unit,
+    '+',
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'+', 
+    List<'.',
+    Unit
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>;
+
+
+using res = typename Step<Program, Tape<>>::type;
