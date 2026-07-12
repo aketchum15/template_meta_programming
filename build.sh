1a0c8311017f0b8cf6a00738c55a4e266a92ad4a
Adam Ketchum <adam.ketchum@att.net>
2026-07-12 18:54:24 -0400

    std version

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ if [ ! -d "out/" ]; then
     mkdir out
 fi
 
-gcc -o out/hello_world -I./src/ -Os -fno-exceptions -fno-rtti -s -ftemplate-depth=10000 brainfuck/HelloWorld.cxx 
+gcc -o out/hello_world -I./src/ -std=c++11 -Os -fno-exceptions -fno-rtti -s -ftemplate-depth=10000 brainfuck/HelloWorld.cxx 
